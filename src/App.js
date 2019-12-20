cdc6c03af2cd47d2ab444d3e3416af2f1cb99759
Jeffrey <jeffreymartespolanco@hotmail.com>
2019-12-20 13:14:43 -0800

    80% done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterCard from './components/CharacterCard'
+import CharacterList from "./components/CharacterList.js";
+import { Route } from "react-router-dom";
+import Characters from "./components/Characters.js";
+import WelcomePage from "./components/WelcomePage.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default function App() {
   return (
-    <main>
+    <main className = 'character-list'>
+      <SearchForm/>
+      <Route exact path = '/'>
       <Header />
+       </Route>
+       <Route exact path = '/'>
+         
+         <CharacterList/>
+       </Route>
+       <Route path = '/characters/:charID'>
+         <WelcomePage/>
+         <Characters/>
+
+         </Route>
+      
+     
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
+import CharacterList from './CharacterList'
+export default function SearchForm({passData}) {
+  const [name, setName] = useState("");
 
-export default function SearchForm() {
- 
+
+  const characters = {passData}
+  console.log(characters)
+  const handleSubmit = event => {
+    event.preventDefault();
+    
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit ={event=> handleSubmit(event)}>
+        <label>
+          Search for a character
+          <input type="text"></input>
+        </label>
+      </form>
     </section>
   );
 }
